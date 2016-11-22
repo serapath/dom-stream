631d6ea6fe05e21a6895c97130d9edf53b0e6623
serapath <dev@serapath.de>
2016-11-22 17:33:10 +0100

    ADD dom-stream

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # dom-stream
-stream dom elements
+stream a dom element into a dom element container
 
 # usage
 `npm install dom-stream`
 
 ```js
+var bel = require('bel')
 var domstream = require('dom-stream')
 
-// experimental
+var container$ = domStream(bel`<div class='container'></div>`)
+
+// container$ is a dom element
+document.body.appendChild(container$)
+// container$ is a stream too
+container.write(bel`<h1>hello foo</h1>`)
+setTimeout(funcion () {
+  container.write(bel`<h1>hello bar</h1>`)
+},1000)
 ```
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-throw new Error('experimental - do not use yet')
+var xDom = require('x-is-dom')
+var read  = require('readable-stream')
+var write = read.Writable
 
-module.exports = domstream
-
-function domstream () {
-  
+module.exports = domStream
+/******************************************************************************
+  DOM STREAM
+******************************************************************************/
+function domStream (container) {
+  container = container || document.createElement('div')
+  if (!xDom(container)) throw Error('arguments[0] must be a dom element')
+  var write$ = write({ objectMode: true })
+  for (key in write$) { container[key] = write$[key] }
+  container._write = function (element, encoding, next) {
+    container.innerHTML = undefined
+    container.appendChild(element) // @IDEA improve perf with raf?
+    next()
+  }
+  return container
 }
